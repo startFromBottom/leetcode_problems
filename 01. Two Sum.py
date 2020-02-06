8ccaa50bb82619d4afcd28c2f281741ec5718180
startFromBottom <uhh0701@gmail.com>
2020-02-06 22:01:24 +0900

    add O(n) Solution in Two Sum problem

--- 01. Two Sum.py
+++ 01. Two Sum.py
@@ -7,12 +7,34 @@ submission detail : https://leetcode.com/submissions/detail/288925577/
 
 from typing import List
 
+
 class Solution:
+    """
+    O(n**2) Solution
+    """
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+        return False
+
+
+# 19.02.06 upload
+class Solution:
+    """
+    O(n) Solution
+    """
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        dict = {}
+        for i, num in enumerate(nums):
+            if target - num in dict:
+                return dict[target - num], i
+            dict[num] = i
 
         return False
--- 07. Reverse Integer.py
+++
@@ -1,8 +0,0 @@
-"""
-
-problem link : https://leetcode.com/problems/reverse-integer/
-submission detail :
-
-"""
-
-print(int("000021"))
