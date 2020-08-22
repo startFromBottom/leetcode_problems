fda59f39a299927f86caf622ee654e196387fa55
startFromBottom <uhh0701@gmail.com>
2020-08-22 15:03:04 +0900

    add 110. Balanced Binary Tree.py

---
+++ 110. Balanced Binary Tree.py
@@ -0,0 +1,31 @@
+"""
+
+problem link : https://leetcode.com/problems/balanced-binary-tree/
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+
+        def check(root: TreeNode) -> int:
+            if not root:
+                return 0
+
+            left = check(root.left)
+            right = check(root.right)
+
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+
+            return max(left, right) + 1
+
+        return check(root) != -1
